257f929ee3ceaffdbc98d3db23b0a9ce4d93e8a3
Aryan Sharma <aryansharma98g@gmail.com>
2025-12-31 12:51:57 +0530

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,37 +1,56 @@
-const CACHE_NAME = "as-editor-v1";
-const ASSETS = [
-    "./",
-    "./index.html",
-    "./manifest.json",
-    "https://cdn-icons-png.flaticon.com/512/1005/1005141.png"
+// A.S Code Service Worker (Network-First Strategy)
+const CACHE_NAME = 'as-code-v3-final';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
 ];
 
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
+// 1. Install Event
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener("activate", (e) => {
-    e.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
+// 2. Fetch Event (Network First)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // If we found the file online, return it AND cache it
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+        return response;
+      })
+      .catch(() => {
+        // If offline, use the cache
+        return caches.match(event.request);
+      })
+  );
 });
 
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
+// 3. Activate Event (Cleanup)
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      );
+    })
+  );
 });
